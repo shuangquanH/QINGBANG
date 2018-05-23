f5686c42c3cf5a77d12416e4cd7d546ea2f9bbb8
qwuser <qwuser@192.168.1.118>
2018-05-23 19:30:23 +0800

    首页

--- QingYouProject/YGClasses/Classes/Home/SQHomeViewController.m
+++ QingYouProject/YGClasses/Classes/Home/SQHomeViewController.m
@@ -9,10 +9,11 @@
 #import "SQHomeViewController.h"
 #import "SQOvalFuncButtons.h"
 #import "SQCardScrollView.h"
+#import "SQCollectionViewLayout.h"
 
-@interface SQHomeViewController () <SQOvalFuncButtonDelegate>
+@interface SQHomeViewController () <SQOvalFuncButtonDelegate, SQCollectionViewLayoutDelegate, UICollectionViewDataSource>
 
-@property (nonatomic, strong) UICollectionView        *collectionView;
+@property (nonatomic, strong) SQBaseCollectionView        *collectionView;
 
 @end
 
@@ -22,10 +23,20 @@
     [super viewDidLoad];
     self.naviTitle = @"首页";
     [self.view addSubview:self.collectionView];
-    
-    
     [self usedOvalView];
     
+    
+    
+}
+
+
+- (void)usedOvalView {
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 400, YGScreenWidth)];
+    
+    CGRect frame = CGRectMake((YGScreenWidth-260)/2.0, 20, 280, 220);
+    SQOvalFuncButtons   *view = [[SQOvalFuncButtons alloc] initWithFrame:frame centBtnSize:CGSizeMake(30, 30) backImage:[UIImage imageNamed:@"ovalimage"]];
+    view.delegate = self;
+    [headerView addSubview:view];
     
     NSMutableArray  *btnArr = [NSMutableArray array];
     NSArray *btnTitle = @[@"水电缴费", @"会议室预定", @"办公耗材",
@@ -41,35 +52,62 @@
         [button addTarget:self action:@selector(btnaction) forControlEvents:UIControlEventTouchUpInside];
     }
     
-    CGRect frame = CGRectMake(0, 400, YGScreenWidth, 100);
-    SQCardScrollView    *scrollview = [[SQCardScrollView alloc] initWithFrame:frame];
+    CGRect frame2 = CGRectMake(0, view.sqbottom+20, YGScreenWidth, 100);
+    SQCardScrollView    *scrollview = [[SQCardScrollView alloc] initWithFrame:frame2];
     [self.view addSubview:scrollview];
     [scrollview setItemArr:btnArr alongAxis:SQSAxisTypeHorizontal spacing:20 leadSpace:0 tailSpace:0 itemSize:CGSizeMake(200, 100)];
-}
-- (void)btnaction {
-    NSLog(@"dd");
-}
 
-- (void)usedOvalView {
-    CGRect frame = CGRectMake((YGScreenWidth-260)/2.0, 60, 280, 220);
-    SQOvalFuncButtons   *view = [[SQOvalFuncButtons alloc] initWithFrame:frame centBtnSize:CGSizeMake(30, 30) backImage:[UIImage imageNamed:@"ovalimage"]];
-    view.delegate = self;
-    [self.view addSubview:view];
+    [headerView addSubview:scrollview];
+    
+    self.collectionView.headerView = headerView;
 }
 - (void)didselectWithClicktype:(ClickType)type {
     NSLog(@"%lu", (unsigned long)type);
 }
 
 
+- (void)btnaction {
+    NSLog(@"dd");
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    SQBaseCollectionViewCell *cell = [SQBaseCollectionViewCell cellWithCollectionView:collectionView forIndexPath:indexPath];
+    UILabel *label = [[UILabel alloc] initWithFrame:cell.bounds];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.text = [NSString stringWithFormat:@"%ld", indexPath.row];
+    [cell addSubview:label];
+    cell.backgroundColor = colorWithMainColor;
+    return cell;
+}
+- (CGSize)waterfallLayout:(SQCollectionViewLayout *)waterfallLayout atIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row%3==0) {
+        return CGSizeMake(100, 100);
+    } else if (indexPath.row%7==0) {
+        return CGSizeMake(100, 50);
+    } else {
+        return CGSizeMake(100, 200);
+    }
+}
+
 
 
 
 
 #pragma lazyLoad
-- (UICollectionView *)collectionView {
+- (SQBaseCollectionView *)collectionView {
     if (!_collectionView) {
-        UICollectionViewLayout *layout = [[UICollectionViewLayout alloc] init];
-        _collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+        SQCollectionViewLayout *layout = [SQCollectionViewLayout waterFallLayoutWithColumnCount:2];
+        [layout setColumnSpacing:10 rowSpacing:10 sectionInset:UIEdgeInsetsMake(10, 10, 10, 10)];
+        
+        _collectionView = [[SQBaseCollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+        _collectionView.backgroundColor = kWhiteColor;
+        _collectionView.dataSource = self;
+        layout.delegate = self;
     }
     return _collectionView;
 }
