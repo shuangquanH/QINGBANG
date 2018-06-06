deb2aa8fdc325471108b3b283d3cfba7953ef337
weikun <845188093@qq.com>
2018-06-06 08:51:10 +0800

    添加设置界面

---
+++ QingYouProject/Classes/UserCenter/setting/PasswordSetValidateController.m
@@ -0,0 +1,156 @@
+//
+//  PasswordSetValidateController.m
+//  QingYouProject
+//
+//  Created by zhaoao on 2017/11/8.
+//  Copyright © 2017年 ccyouge. All rights reserved.
+//
+
+#import "PasswordSetValidateController.h"
+#import "PasswordModifilyViewController.h"
+
+@interface PasswordSetValidateController ()
+{
+    UIView *_verifyBaseView;
+}
+@property(nonatomic,strong)UITextField *verifyTextField;
+@property(nonatomic,strong) UIButton *verifyButton;
+@end
+
+@implementation PasswordSetValidateController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.naviTitle = @"密码设置";
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self registerTimerNotification];
+    [self configUI];
+}
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:YES];
+    
+}
+
+-(void)configUI
+{
+    //提示文字
+    UILabel *tipLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 30, YGScreenWidth, 20)];
+    tipLabel.textAlignment = NSTextAlignmentCenter;
+    tipLabel.textColor = colorWithLightGray;
+    tipLabel.font = [UIFont systemFontOfSize:14.0];
+    tipLabel.text = [NSString stringWithFormat:@"设置密码需要验证您的手机号%@****%@",[YGSingletonMarco.user.phone substringWithRange:NSMakeRange(0, 3)],[YGSingletonMarco.user.phone substringWithRange:NSMakeRange(7, 4)]];
+    [self.view addSubview:tipLabel];
+    
+    [self createVerifyCodeViewWithBaseView];
+}
+
+#pragma 验证码
+- (void)createVerifyCodeViewWithBaseView
+{
+    //baseview
+    _verifyBaseView = [[UIView alloc] initWithFrame:CGRectMake(15, 100, YGScreenWidth - 30, 50)];
+    [self.view addSubview:_verifyBaseView];
+    //验证码图
+    UIImageView *verifyImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"login_captcha_icon_black"]];
+    [verifyImageView sizeToFit];
+    verifyImageView.frame = CGRectMake(10, 10, verifyImageView.width, verifyImageView.height);
+    //    verifyImageView.centery = _verifyBaseView.centery;
+    [_verifyBaseView addSubview:verifyImageView];
+    
+    //手机号text
+    _verifyTextField = [[UITextField alloc] initWithFrame:CGRectMake(verifyImageView.x + verifyImageView.width + 15, 10, _verifyBaseView.width - verifyImageView.x - verifyImageView.width - 130, 30)];
+    _verifyTextField.centery = verifyImageView.centery;
+    _verifyTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    _verifyTextField.font =  [UIFont systemFontOfSize:YGFontSizeBigOne];
+    _verifyTextField.textColor = colorWithBlack;
+    _verifyTextField.placeholder = @"验证码";
+    [_verifyTextField setValue:colorWithPlaceholder forKeyPath:@"_placeholderLabel.textColor"];
+    _verifyTextField.keyboardType = UIKeyboardTypeNumberPad;
+    [_verifyBaseView addSubview:_verifyTextField];
+    
+    //发送验证码
+    _verifyButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_verifyButton setTitle:@"发送验证码" forState:UIControlStateNormal];
+    [_verifyButton setTitleColor:colorWithMainColor forState:UIControlStateNormal];
+    _verifyButton.titleLabel.font = [UIFont systemFontOfSize:YGFontSizeNormal];
+    _verifyButton.frame = CGRectMake(_verifyBaseView.width -110, 0, 100, 35);
+    _verifyButton.layer.cornerRadius = 17;
+    _verifyButton.layer.borderColor = colorWithMainColor.CGColor;
+    _verifyButton.layer.borderWidth = 1;
+    [_verifyButton addTarget:self action:@selector(verifyButtonAction) forControlEvents:UIControlEventTouchUpInside];
+    [_verifyBaseView addSubview:_verifyButton];
+    
+    //线
+    UIView *lineView1 = [[UIView alloc] initWithFrame:CGRectMake(0, _verifyTextField.y + _verifyTextField.height + 10, _verifyBaseView.width, 0.5)];
+    lineView1.backgroundColor = colorWithLine;
+    [_verifyBaseView addSubview:lineView1];
+    
+    UIButton *nextButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    nextButton.frame = CGRectMake(15, 200, YGScreenWidth - 30, 40);
+    nextButton.backgroundColor = colorWithMainColor;
+    nextButton.layer.cornerRadius = 20;
+    nextButton.clipsToBounds = YES;
+    [nextButton setTitle:@"下一步" forState:UIControlStateNormal];
+    [nextButton addTarget:self action:@selector(nextClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:nextButton];
+}
+-(void)verifyButtonAction
+{
+    self.verifyButton.enabled = NO;
+    [YGNetService showLoadingViewWithSuperView:self.view];
+    [self startPostWithURLString:REQUEST_sendSMS parameters:@{@"phone":YGSingletonMarco.user.phone,@"type":@"1"} showLoadingView:NO scrollView:nil];
+
+}
+- (void)didReceiveSuccessResponeseWithURLString:(NSString *)URLString parameters:(id)parameters responeseObject:(id)responseObject
+{
+    [YGNetService dissmissLoadingView];
+    [[YGSingleton sharedManager] startTimerWithTime:60];
+    [YGAppTool showToastWithText:@"验证码发送成功，请注意查收"];
+    [self.verifyTextField becomeFirstResponder];
+}
+- (void)didReceiveFailureResponeseWithURLString:(NSString *)URLString parameters:(id)parameters error:(NSError *)error
+{
+    [YGNetService dissmissLoadingView];
+    self.verifyButton.enabled = YES;
+}
+- (void)commonTimerCountingDownWithLeftSeconds:(NSInteger)seconds
+{
+    [self.verifyButton setTitleColor:colorWithPlaceholder forState:UIControlStateNormal];
+    self.verifyButton.layer.borderColor = colorWithPlaceholder.CGColor;
+    [self.verifyButton setTitle:[NSString stringWithFormat:@"重新获取(%lu)", (unsigned long) seconds] forState:UIControlStateNormal];
+}
+
+- (void)commonTimerDidFinishCountDown
+{
+    self.verifyButton.enabled = YES;
+    [self.verifyButton setTitleColor:colorWithMainColor forState:UIControlStateNormal];
+    self.verifyButton.layer.borderColor = colorWithMainColor.CGColor;
+    [self.verifyButton setTitle:@"发送验证码" forState:UIControlStateNormal];
+}
+
+-(void)nextClick:(UIButton *)button
+{
+    if (!_verifyTextField.text.length) {
+        [YGAppTool showToastWithText:@"请输入验证码哦"];
+        return;
+    }
+
+    [YGNetService YGPOST:REQUEST_checkSMS parameters:@{@"phone":YGSingletonMarco.user.phone,@"type":@"1",@"code":self.verifyTextField.text} showLoadingView:NO scrollView:nil success:^(id responseObject) {
+        
+        NSLog(@"%@",responseObject);
+        
+        PasswordModifilyViewController *vc = [[PasswordModifilyViewController alloc]init];
+        [self.navigationController pushViewController:vc animated:YES];
+        
+    } failure:^(NSError *error) {
+    }];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
---
+++ QingYouProject/Classes/UserCenter/setting/WKSettingViewController.m
@@ -0,0 +1,235 @@
+//
+//  SetupViewController.m
+//  QingYouProject
+//
+//  Created by zhaoao on 2017/11/8.
+//  Copyright © 2017年 ccyouge. All rights reserved.
+//
+
+#import "WKSettingViewController.h"
+#import "SQTicketApplyListViewController.h"
+#import "LoginViewController.h"
+#import "PasswordSetValidateController.h"
+#import "AboutusViewController.h"
+#import "ManageMailPostViewController.h"
+#import "EvaluateViewController.h"
+#import "YGPushSDK.h"
+
+@interface WKSettingViewController () <UITableViewDelegate,UITableViewDataSource>
+{
+    UITableView *_tableView;
+    NSArray *_titleArray;
+}
+
+@end
+
+@implementation WKSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.naviTitle = @"设置";
+    self.view.backgroundColor = colorWithTable;
+    [self configUI];
+}
+-(void)configUI
+{
+    if(YGSingletonMarco.user.userId.length)
+    {
+         _titleArray = [NSArray arrayWithObjects:@"手机号",@"密码设置",@"收货地址",@"发票抬头",@"清除缓存",@"反馈",@"关于我们", nil];
+    }
+    else
+    {
+        _titleArray = [NSArray arrayWithObjects:@"清除缓存",@"关于我们", nil];
+    }
+   
+    _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, YGScreenWidth, 300) style:UITableViewStyleGrouped];
+    _tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, YGScreenWidth, 0.001)];
+    _tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, YGScreenWidth, 0.001)];
+    _tableView.sectionHeaderHeight = 0.001;
+    _tableView.sectionFooterHeight = 0.001;
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.scrollEnabled = NO;
+    [self.view addSubview:_tableView];
+    _tableView.backgroundColor = colorWithTable;
+    
+    if(YGSingletonMarco.user.userId.length)
+    {
+        UIButton *exitButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        exitButton.backgroundColor = [UIColor whiteColor];
+        exitButton.frame = CGRectMake(0, 350, YGScreenWidth, 50);
+        [exitButton setTitle:@"退出登录" forState:UIControlStateNormal];
+        [exitButton setTitleColor:colorWithOrangeColor forState:UIControlStateNormal];
+        [exitButton addTarget:self action:@selector(exitClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:exitButton];
+        exitButton.layer.borderWidth = 1;
+        exitButton.layer.borderColor = colorWithLine.CGColor;
+        
+    }
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"cell"];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        cell.textLabel.font = [UIFont systemFontOfSize:15.0];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+    cell.textLabel.text = _titleArray[indexPath.row];
+    if ([cell.textLabel.text isEqualToString:@"清除缓存"]) {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        NSInteger memory = (int)[self filePath];
+        NSString * memoryString = [NSString stringWithFormat:@"%ldM",memory];;
+        cell.detailTextLabel.text = memoryString;
+    }
+    if ([cell.textLabel.text isEqualToString:@"手机号"]) {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        UILabel *phoneLabel = [[UILabel alloc]initWithFrame:CGRectMake(YGScreenWidth - 300, 0, 285, 50)];
+        phoneLabel.textAlignment = NSTextAlignmentRight;
+        phoneLabel.text = YGSingletonMarco.user.phone;
+        [cell.contentView addSubview:phoneLabel];
+    }
+    return cell;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _titleArray.count;
+}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(!YGSingletonMarco.user.userId.length)
+    {
+        if (indexPath.row == 0) {
+            [self clearFile];
+        }
+        if (indexPath.row == 1) {
+            AboutusViewController *vc = [[AboutusViewController alloc]init];
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+        return;
+        
+    }
+    else
+    {
+        if (indexPath.row == 1) {
+            PasswordSetValidateController *vc = [[PasswordSetValidateController alloc]init];
+            [self.navigationController pushViewController:vc animated:YES];
+            return;
+        }
+        if(indexPath.row == 2)
+        {
+            ManageMailPostViewController *vc = [[ManageMailPostViewController alloc] init];
+            vc.pageType = @"personCenter";
+            [self.navigationController pushViewController:vc animated:YES];
+            return;
+        }
+        if (indexPath.row == 3) {
+            SQTicketApplyListViewController *next = [[SQTicketApplyListViewController alloc] initWithIsTicketApplyManager:YES];
+            [self.navigationController pushViewController:next animated:YES];
+            return;
+        }
+        if(indexPath.row == 4)
+        {
+            [self clearFile];
+            return;
+        }
+        if (indexPath.row == 5) {
+            EvaluateViewController *vc = [[EvaluateViewController alloc]init];
+            [self.navigationController pushViewController:vc animated:YES];
+            return;
+        }
+        if (indexPath.row == 6) {
+            AboutusViewController *vc = [[AboutusViewController alloc]init];
+            [self.navigationController pushViewController:vc animated:YES];
+            return;
+        }
+    }
+}
+
+// 显示缓存大小
+-(float)filePath
+{
+    NSString * cachPath = [NSSearchPathForDirectoriesInDomains ( NSCachesDirectory,NSUserDomainMask,YES) firstObject];
+    return [ self folderSizeAtPath :cachPath];
+}
+//1:首先我们计算一下 单个文件的大小
+- ( float ) folderSizeAtPath:( NSString *) folderPath{
+    NSFileManager * manager = [ NSFileManager defaultManager ];
+    if (![manager fileExistsAtPath :folderPath]) return 0 ;
+    NSEnumerator *childFilesEnumerator = [[manager subpathsAtPath:folderPath] objectEnumerator];
+    NSString * fileName;
+    long long folderSize = 0 ;
+    while ((fileName = [childFilesEnumerator nextObject ]) != nil ){
+        NSString * fileAbsolutePath = [folderPath stringByAppendingPathComponent :fileName];
+        folderSize += [self fileSizeAtPath:fileAbsolutePath];
+    }
+    return folderSize/( 1024.0 * 1024.0 );
+}
+//2:遍历文件夹获得文件夹大小，返回多少 M（提示：你可以在工程界设置（)m）
+- ( long long) fileSizeAtPath:(NSString *) filePath{
+    NSFileManager * manager = [NSFileManager defaultManager];
+    if ([manager fileExistsAtPath:filePath]){
+        return [[manager attributesOfItemAtPath:filePath error:nil] fileSize ];
+    }
+    return 0 ;
+}
+
+//清除缓存
+- (void)clearFile
+{
+    NSString * cachPath = [NSSearchPathForDirectoriesInDomains ( NSCachesDirectory,NSUserDomainMask,YES) firstObject];
+    NSArray * files = [[NSFileManager defaultManager] subpathsAtPath:cachPath];
+    NSLog (@"cachpath = %@",cachPath);
+    for ( NSString * p in files) {
+        NSError * error = nil;
+        NSString * path = [cachPath stringByAppendingPathComponent:p];
+        if ([[NSFileManager defaultManager]fileExistsAtPath:path]) {
+            [[NSFileManager defaultManager]removeItemAtPath:path error:&error];
+        }
+    }
+    [self performSelectorOnMainThread:@selector(clearCachSuccess)withObject : nil waitUntilDone:YES ];
+}
+-(void)clearCachSuccess
+{
+    NSIndexPath *index;
+    if(!YGSingletonMarco.user.userId.length)
+    {
+        index = [NSIndexPath indexPathForRow:0 inSection:0];//刷新
+    }else
+    {
+        index = [NSIndexPath indexPathForRow:3 inSection:0];//刷新
+    }
+    
+    [_tableView reloadRowsAtIndexPaths:[NSArray arrayWithObjects:index,nil] withRowAnimation:UITableViewRowAnimationNone];
+    [YGAppTool showToastWithText:@"清除成功!"];
+}
+
+
+
+
+-(void)exitClick:(UIButton *)button
+{
+    if (YGSingletonMarco.deviceToken)
+    {
+        [YGPushSDK unbindSDKWithUserId:YGSingletonMarco.user.userId];
+    }
+    [YGSingletonMarco deleteUser];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+@end
