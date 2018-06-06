fde18d7a29363952c40f0d789f549686ba390913
qwuser <qwuser@192.168.1.118>
2018-06-06 17:15:37 +0800

    确认订单页面

---
+++ QingYouProject/Classes/Home/DecorationServe/SQChooseDecorationAddressView.h
@@ -0,0 +1,13 @@
+//
+//  SQChooseDecorationAddressView.h
+//  QingYouProject
+//
+//  Created by qwuser on 2018/6/6.
+//  Copyright © 2018年 ccyouge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQChooseDecorationAddressView : UIView
+
+@end
---
+++ QingYouProject/Classes/Home/DecorationServe/SQChooseDecorationAddressView.m
@@ -0,0 +1,34 @@
+//
+//  SQChooseDecorationAddressView.m
+//  QingYouProject
+//
+//  Created by qwuser on 2018/6/6.
+//  Copyright © 2018年 ccyouge. All rights reserved.
+//
+
+#import "SQChooseDecorationAddressView.h"
+
+@implementation SQChooseDecorationAddressView {
+    UILabel *addAddressLabel;
+    
+}
+
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
