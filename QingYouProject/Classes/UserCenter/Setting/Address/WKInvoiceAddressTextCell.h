9eabcc207fb8eeb1e638419604cd17400680ce2c
weikun <845188093@qq.com>
2018-06-26 14:00:57 +0800

    修改地址列表样式

---
+++ QingYouProject/Classes/UserCenter/Setting/Address/WKInvoiceAddressTextCell.h
@@ -0,0 +1,20 @@
+//
+//  WKInvoiceAddressTextCell.h
+//  QingYouProject
+//
+//  Created by mac on 2018/6/26.
+//  Copyright © 2018年 ccyouge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WKInvoiceAddressModel;
+@class ManageMailPostModel;
+
+@interface WKInvoiceAddressTextCell : UITableViewCell
+
+- (void)configAddress:(WKInvoiceAddressModel *)addressInfo;
+
+- (void)configMailAddress:(ManageMailPostModel *)addressInfo;
+
+@end
