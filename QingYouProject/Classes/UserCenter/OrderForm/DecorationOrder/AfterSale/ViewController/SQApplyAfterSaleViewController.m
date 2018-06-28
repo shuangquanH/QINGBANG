8509e011a4f81c19389042ce12d2162e0dc904b8
weikun <845188093@qq.com>
2018-06-28 10:03:39 +0800

    售后条件限制

--- QingYouProject/Classes/UserCenter/OrderForm/DecorationOrder/AfterSale/ViewController/SQApplyAfterSaleViewController.m
+++ QingYouProject/Classes/UserCenter/OrderForm/DecorationOrder/AfterSale/ViewController/SQApplyAfterSaleViewController.m
@@ -150,6 +150,7 @@
     
     NSMutableArray *tmp = [self.navigationController.viewControllers mutableCopy];
     [tmp removeObject:self];
+    [self.navigationController setValue:[tmp copy] forKey:@"viewControllers"];
 }
 
 - (void)click_confirmButton {
@@ -159,18 +160,20 @@
         return;
     }
     
-    if (!_afterSaleImageArray.count) {
-        [YGAppTool showToastWithText:@"请至少添加一张凭证图片"];
-        return;
-    }
-
-    [YGNetService showLoadingViewWithSuperView:YGAppDelegate.window];
     NSMutableArray *total = [NSMutableArray array];
     for (id obj in self.afterSaleImageArray) {
         if ([obj isKindOfClass:[UIImage class]]) {
             [total addObject:obj];
         }
     }
+    
+    if (!total.count) {
+        [YGAppTool showToastWithText:@"请至少添加一张凭证图片"];
+        return;
+    }
+
+    [YGNetService showLoadingViewWithSuperView:YGAppDelegate.window];
+    
     [SQRequest uploadImages:total param:nil progress:^(float progress) {
         
     } success:^(id response) {
